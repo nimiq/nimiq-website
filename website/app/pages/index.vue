a46cf435a109d3730eb183d9313038a8a5c54ee1
onmax <maximogarciamtnez@gmail.com>
2024-09-06 09:25:50 +0200

    add precommit

--- website/app/pages/index.vue
+++ website/app/pages/index.vue
@@ -7,11 +7,10 @@ const { data: document } = await useAsyncData('page', async () => {
   const document = await client.getSingle('home')
   if (!document)
     throw createError({ statusCode: 404, message: 'Page not found' })
-  console.log(document)
   return document
 })
 </script>
 
 <template>
-  <slice-zone :components="components" :slices="document!.data.slices" />
+  <slice-zone :components="components" :slices="document.data.slices" />
 </template>
