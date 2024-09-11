d42b3e986aeb6345f47cacf8b29229e99365eb5a
onmax <maximogarciamtnez@gmail.com>
2024-09-11 19:14:00 +0200

    disable ssr for liveview iframe

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -56,6 +56,7 @@ export default defineNuxtConfig({
 
   routeRules: {
     '/api/liveview/policy': { swr: 3600 }, // cache 1 hour
+    '/iframes/albatross-liveview': { ssr: false },
   },
 
   watch: ['server/**/*'],
