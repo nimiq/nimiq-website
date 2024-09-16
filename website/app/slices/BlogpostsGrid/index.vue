49f63a7a63be131e88b9cb1bff1fa57d748b5e29
onmax <maximogarciamtnez@gmail.com>
2024-09-16 11:32:29 +0200

    paginataion in blog

--- website/app/slices/BlogpostsGrid/index.vue
+++ website/app/slices/BlogpostsGrid/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { filter } from '@prismicio/client'
 import type { Content } from '@prismicio/client'
 import type { BlogPageDocument } from '~~/prismicio-types'
+import { filter } from '@prismicio/client'
 import ArticleMetadata from '~/components/ArticleMetadata.vue'
 
 const props = defineProps(getSliceComponentProps<Content.BlogpostsGridSlice>())
@@ -9,12 +9,21 @@ const { sectionRef } = useSlice(props.slice.id, 'grey')
 
 const showDrafts = true // TODO Change this depending on the NODE_ENV
 
-const { data } = usePrismicDocumentsByType<BlogPageDocument>('blog_page', {
-  orderings: { field: 'my.blog_page.publish_date', direction: 'desc' },
-  filters: showDrafts ? undefined : [filter.not('my.blog_page.draft', true)],
-  pageSize: 20,
+const itemsPerPage = 25
+const page = useRouteQuery('page', 1)
+
+const { client } = usePrismic()
+
+const { data: result } = useAsyncData('blog_posts', async () => {
+  return await client.getByType('blog_page', {
+    orderings: { field: 'my.blog_page.publish_date', direction: 'desc' },
+    filters: showDrafts ? undefined : [filter.not('my.blog_page.draft', true)],
+    pageSize: itemsPerPage,
+    page: page.value,
+  })
 })
-const posts = computed(() => data.value?.results || [])
+const posts = computed(() => result.value?.results ?? [])
+const totalPages = computed(() => result.value?.total_pages ?? 0)
 
 function getAbstract(post: BlogPageDocument): string {
   const { prose } = useProse(post)
@@ -23,9 +32,9 @@ function getAbstract(post: BlogPageDocument): string {
 </script>
 
 <template>
-  <section v-if="posts.length > 0" ref="sectionRef" grid="~ cols-1 md:cols-2 lg:cols-3 gap-16">
-    <article v-for="(post, i) in posts" :key="i" :class="{ 'md:self-end': i === 1, 'md:self-stretch': i > 1 }" md:first:col-span-2>
-      <NuxtLink :to="post.uid" relative h-full nq-hoverable>
+  <section ref="sectionRef" grid="~ cols-1 md:cols-2 lg:cols-3 gap-16">
+    <article v-for="(post, i) in posts" :key="post.id" :class="{ 'md:self-end': i === 1, 'md:self-stretch': i > 1 }" md:first:col-span-2>
+      <NuxtLink :to="`/blog/${post.uid}`" relative h-full nq-hoverable>
         <div v-if="post.data.draft" absolute right-12 top-12 ring="1.5 white" nq-pill-orange>
           <div i-nimiq:locked-lock />
           Draft
@@ -42,5 +51,34 @@ function getAbstract(post: BlogPageDocument): string {
         </div>
       </NuxtLink>
     </article>
+    {{ page }}
+    <PaginationRoot v-model:page="page" :total="totalPages * itemsPerPage" :items-per-page show-edges col-span-full mt-32>
+      <PaginationList v-slot="{ items }" flex="~ gap-16 items-center justify-center">
+        <PaginationPrev class="item">
+          <div i-nimiq:chevron-left text-9 op-70 />
+        </PaginationPrev>
+        <template v-for="(page, index) in items">
+          <PaginationListItem v-if="page.type === 'page'" :key="index" class="item" :value="page.value">
+            {{ page.value }}
+          </PaginationListItem>
+          <PaginationEllipsis v-else :key="page.type" :index="index" class="item">
+            &#8230;
+          </PaginationEllipsis>
+        </template>
+        <PaginationNext class="item">
+          <div i-nimiq:chevron-right text-9 op-70 />
+        </PaginationNext>
+      </PaginationList>
+    </PaginationRoot>
   </section>
 </template>
+
+<style scoped>
+  .item {
+  --uno: 'rounded-4 size-32 shrink-0 bg-neutral-100 text-neutral-900 text-12 font-semibold hocus:bg-neutral-200 transition-colors ring-1.5 ring-neutral-400 flex items-center justify-center';
+  &[data-selected] {
+    --uno: 'bg-blue text-white ring-none';
+  }
+}
+</style>
+}
