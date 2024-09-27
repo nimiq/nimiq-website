558c4acb4e6f48be79374c50c9d5a361d7f4eeca
onmax <maximogarciamtnez@gmail.com>
2024-09-27 17:50:19 +0200

    added border top to pill links if following section with same color

--- app/slices/PillLink/index.vue
+++ app/slices/PillLink/index.vue
@@ -2,23 +2,63 @@
 import type { Content } from '@prismicio/client'
 import type { PillLinkSliceDefaultPrimary } from '~~/prismicio-types'
 
-const props = defineProps(getSliceComponentProps<Content.PillLinkSlice>())
-const slices = toRef(props, 'slices')
-const slice = toRef(props, 'slice')
-const items = slices.value
+type Item = Exclude<PillLinkSliceDefaultPrimary['item'], null>
+
+const { slice, slices } = defineProps(getSliceComponentProps<Content.PillLinkSlice>())
+
+const items = slices
   .filter(slice => slice.slice_type === 'pill_link')
   .map(slice => (slice as unknown as { primary: PillLinkSliceDefaultPrimary }).primary)
   .map(primary => ({
     item: primary.item!,
     label: primary.label!,
-    active: slice.value.primary.item === primary.item,
+    active: slice.primary.item === primary.item,
   }))
 
-const sectionRef = useSection(props.slice.id, slice.value.primary.bgColor, { limitWidth: false, forceGap: true })
+const sectionRef = useSection(slice.id, slice.primary.bgColor, { limitWidth: false, forceGap: true })
+
+// @unocss-include
+
+const colors: Record<Item, string> = {
+  'The Tech': 'text-gold bg-gold/20',
+  'The Apps': 'text-green bg-green/20',
+  'The Map': 'text-orange bg-orange/20',
+}
+
+const icons: Record<Item, string> = {
+  'The Tech': 'i-nimiq:bolt',
+  'The Apps': 'i-nimiq:arrows-to-sides',
+  'The Map': 'i-nimiq:crypto-map',
+}
+
+const activeItem = computed(() => items.filter(({ active }) => active)[0]?.item)
+
+const strToUrl = (str: string) => str.toLowerCase().replace(/ /g, '-')
 </script>
 
 <template>
   <section ref="sectionRef" :data-slice="slice" relative max-md:items-start>
-    <PillLinks :items />
+    <!-- This element is just to avoid the user clicking on the menu and seeing the tab hidden behind the header -->
+    <div :id="strToUrl(activeItem!)" sr-only bottom="md:140" />
+    <ul flex="~ items-center md:gap-12 md:justify-center" my-1.5>
+      <li v-for="({ item, label, active }, i) in items" :key="item" flex="~ items-center gap-12">
+        <NuxtLink :to="`#${strToUrl(item)}`" flex="~ items-center gap-10" rounded-full py-6 :class="active ? 'mr-8 pl-8 pr-20 ring-1.5 ring-neutral-500' : 'max-lg:hidden px-8'">
+          <div v-if="active" :class="colors[item]" aria-hidden size-28 rounded-full grid="~ place-content-center">
+            <div :class="icons[item]" h-18 w-16 />
+          </div>
+          <span text="18 neutral-700" nq-label>{{ label }}</span>
+        </NuxtLink>
+        <div v-if="i < items.length - 1" aria-hidden i-nimiq:chevron-right max-lg:hidden text="12 neutral-600" />
+      </li>
+    </ul>
   </section>
 </template>
+
+<style>
+section.darkblue + :is(section[data-slice-type='pill_link']).darkblue,
+section.grey + :is(section[data-slice-type='pill_link']).grey,
+section.white + :is(section[data-slice-type='pill_link']).white {
+  /* Add a border if the pill links are following another section with the same color */
+  --uno: 'border-t border-neutral-500';
+}
+</style>
