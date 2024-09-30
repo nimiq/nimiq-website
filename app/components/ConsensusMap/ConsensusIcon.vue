b4348ad828b541b6bb7f23108b5d3a8e236486ee
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2024-09-30 14:57:35 -0400

    fix(ConsensusIcon): consensus can be null

--- app/components/ConsensusMap/ConsensusIcon.vue
+++ app/components/ConsensusMap/ConsensusIcon.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { NetworkStatus } from '@/types/network'
+import type { ConsensusState } from '@nimiq/core'
 
 defineProps<{
-  consensus: NetworkStatus
+  consensus: ConsensusState | null
 }>()
 </script>
 
 <template>
   <div mt--2px>
-    <div v-if="consensus === NetworkStatus.ESTABLISHED" i-nimiq:world-check />
-    <div v-else-if="consensus === NetworkStatus.CONNECTING || consensus === NetworkStatus.SYNCING" i-nimiq:world />
+    <div v-if="consensus === 'established'" i-nimiq:world-check />
+    <div v-else-if="consensus === 'connecting' || consensus === 'syncing'" i-nimiq:world />
   </div>
 </template>
