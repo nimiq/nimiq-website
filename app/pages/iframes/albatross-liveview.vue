f10a433dc094d8f3dae138defdac19ee566a1394
onmax <maximogarciamtnez@gmail.com>
2024-09-30 08:14:56 +0200

    changes in albatross page

--- app/pages/iframes/albatross-liveview.vue
+++ app/pages/iframes/albatross-liveview.vue
@@ -2,11 +2,11 @@
 const allowSendTxQuery = useRouteQuery('allow-send-tx')
 const allowSendTx = computed(() => allowSendTxQuery.value === 'true')
 
-const networkNotice = 'Showing live Proof of Stake test net data'
+const networkNotice = 'Showing live Proof of Stake testnet data'
 </script>
 
 <template>
-  <NuxtLayout name="iframe" class="dark" bg-neutral-100>
+  <NuxtLayout name="iframe" class="dark" bg-darkblue>
     <Albatross :allow-send-tx :network-notice />
   </NuxtLayout>
 </template>
