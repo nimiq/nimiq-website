e6f4d201f1f720b44929b3578c0754b6ba7e1fdf
onmax <maximogarciamtnez@gmail.com>
2024-10-01 18:21:54 +0200

    updated wording albatross stats

--- app/pages/iframes/albatross-liveview.vue
+++ app/pages/iframes/albatross-liveview.vue
@@ -3,7 +3,9 @@ const allowSendTxQuery = useRouteQuery('allow-send-tx')
 const allowSendTx = computed(() => allowSendTxQuery.value === 'true')
 
 const networkNotice = 'Showing live Proof of Stake testnet data'
-const statsTooltipContent = 'This information is meant solely to project potential NIM staking rewards for Nimiq PoS. The rewards percentage is subject to change based on the actual percentage of NIM  staked compared to the total circulating NIM.'
+const statsTooltipContent = `This information is meant solely to project potential NIM staking rewards for Nimiq PoS. The rewards percentage is subject to change based on the actual percentage of NIM  staked compared to the total circulating NIM.
+
+The content on this website must not be considered financial advice and does not constitute any offer or solicitation to offer or recommendation of any investment product.`
 </script>
 
 <template>
