5050aaeb602ffc31059c65957075515962c37cfb
onmax <maximogarciamtnez@gmail.com>
2024-10-01 16:23:15 +0200

    fix iframe

--- app/pages/iframes/albatross-liveview.vue
+++ app/pages/iframes/albatross-liveview.vue
@@ -3,10 +3,11 @@ const allowSendTxQuery = useRouteQuery('allow-send-tx')
 const allowSendTx = computed(() => allowSendTxQuery.value === 'true')
 
 const networkNotice = 'Showing live Proof of Stake testnet data'
+const statsTooltipContent = 'This information is meant solely to project potential NIM staking rewards for Nimiq PoS. The rewards percentage is subject to change based on the actual percentage of NIM  staked compared to the total circulating NIM.'
 </script>
 
 <template>
   <NuxtLayout name="iframe" class="dark" bg-darkblue>
-    <Albatross :allow-send-tx :network-notice />
+    <Albatross :allow-send-tx :network-notice :stats-tooltip-content />
   </NuxtLayout>
 </template>
