0ddab483125ce86cc8049288f0f0c71360090192
onmax <maximogarciamtnez@gmail.com>
2024-10-03 14:54:34 +0200

    chore(liveview): display curly

--- app/components/Albatross/liveview/Block.vue
+++ app/components/Albatross/liveview/Block.vue
@@ -9,6 +9,6 @@ defineProps<{ block: LiveviewBlock }>()
     <div i-nimiq:arrow-left-thin relative top--8 m-16 font-bold text="16 neutral-600" />
     <AlbatrossLiveviewMacroBlock v-if="block.kind === LiveviewBlockType.MacroBlock" :block />
     <AlbatrossLiveviewMicroBlock v-else-if="block.kind === 'micro'" :block />
-    <div v-else-if="block.kind === 'placeholder'" grid="~ place-content-center" i-custom:curly h-120pt min-w-120pt text="30pt neutral-500" />
+    <div v-else-if="block.kind === 'placeholder'" grid="~ place-content-center" i-custom:curly h-120pt min-w-120pt text="30pt neutral-700/85" />
   </div>
 </template>
