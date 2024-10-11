33fa537c53b6e1e43592093f8ffc8aee692b3227
onmax <maximogarciamtnez@gmail.com>
2024-10-11 08:32:45 +0200

    chore: allow edit roadmap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -126,6 +126,7 @@ export default defineNuxtConfig({
       refreshToken: process.env.NUXT_ZOHO_REFRESH_TOKEN,
       listkey: process.env.NUXT_ZOHO_LISTKEY,
     },
+    siteEnv: process.env.NUXT_SITE_ENV,
   },
 
   hub: {
