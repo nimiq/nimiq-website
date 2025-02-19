92a505875c8200ff9fe912b7263a7cf0bc5e2690
onmax <maximogarciamtnez@gmail.com>
2025-02-19 11:55:47 +0100

    chore: updated UnoCSS and data loaders

--- app/components/Headline/Staking.vue
+++ app/components/Headline/Staking.vue
@@ -29,13 +29,15 @@ const id = `terms-note-${useId()}`
     <component :is="headlineTag" f-m-md text="inverted:white wrap md:balance">
       {{ headlineParts[0] }}
       <span bg="green/15 inverted:white/30" text="green inverted:white" data-percentage rounded-4 px-10 py-3 inline-flex="~">
-        ~{{ rewardPerAnnumPercentage }}<div i-nimiq:asterix translate-y-8 text-14 :aria-labelledby="id" /></span>
+        <!-- TODO Remove asterix once it is in the icons lib -->
+        ~{{ rewardPerAnnumPercentage }}<div i-nimiq:asterisk i-nimiq:asterix translate-y-8 text-14 :aria-labelledby="id" /></span>
       {{ headlineParts[1] }}
     </component>
     <PrismicText v-if="hasText(subline)" wrapper="p" :field="subline" inverted:text="white/80" />
     <PrismicLink v-if="hasLink(cta)" internal-component="a" :field="cta" :class="primaryPill ? 'nq-pill-blue' : 'nq-pill-tertiary'" nq-shadow nq-arrow nq-pill-lg f-m-lg md:mx-auto inverted:shadow-none />
     <small :id text="green-1100 inverted:white/80 sm:center" data-note transition-colors duration-400 f-m-md sm:max-w-32ch>
-      <div aria-hidden i-nimiq:asterix text="7 sm:9" un-translate="x--8 sm:x-4 y-4 sm:y-8" /> {{ stakingValues?.stakingNote }}
+      <!-- TODO Remove asterix once it is in the icons lib -->
+      <div i-nimiq:asterisk aria-hidden i-nimiq:asterix text="7 sm:9" un-translate="x--8 sm:x-4 y-4 sm:y-8" /> {{ stakingValues?.stakingNote }}
     </small>
   </div>
 </template>
@@ -46,28 +48,7 @@ section:has(+ section.gradient-transparent-green-transparent) {
 }
 
 section.gradient-transparent-green {
-  --uno: 'pt-140 lg:pt-152';
-  --gradient-height: 252px;
-  background: linear-gradient(
-    180deg,
-    rgb(var(--nq-neutral-0)) 0px,
-    rgb(var(--nq-neutral-0) / 0.991615) calc(0.1179 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.967585) calc(0.2138 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.9296) calc(0.2912 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.879348) calc(0.3534 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.818519) calc(0.4037 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.7488) calc(0.4456 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.671881) calc(0.4824 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.589452) calc(0.5176 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.5032) calc(0.5544 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.414815) calc(0.5963 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.325985) calc(0.6466 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.2384) calc(0.7088 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.153748) calc(0.7862 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0.0737185) calc(0.8821 * var(--gradient-height)),
-    rgb(var(--nq-neutral-0) / 0) var(--gradient-height)
-  );
-  --uno: 'bg-green';
+  --uno: 'pt-140 lg:pt-152 bg-gradient-fn-from-transparent bg-gradient-fn-to-green bg-gradient-fn-ease-out-cubic bg-gradient-fn-to-b bg-gradient-fn-length-252';
 }
 
 section.gradient-transparent-green-transparent {
--- app/stores/global-content.ts
+++ app/stores/global-content.ts
@@ -1,7 +1,21 @@
-import type { NimiqAppDocument, NimiqAppDocumentData, SocialMediaDocument } from '~~/prismicio-types.js'
+import type {
+  NimiqAppDocument,
+  NimiqAppDocumentData,
+  SocialMediaDocument,
+} from '~~/prismicio-types.js'
+import { useRuntimeConfig, useState } from '#imports'
+import { useQuery } from '@pinia/colada'
+import { defineStore } from 'pinia'
+import { computed } from 'vue'
 
-export type SocialMediaAttributes = SocialMediaDocument['data'] & { color: `#${string}`, icon: string, id: string }
-export type AppsAttributes = NimiqAppDocument['data'] & { color: `#${string}` }
+export type SocialMediaAttributes = SocialMediaDocument['data'] & {
+  color: `#${string}`
+  icon: string
+  id: string
+}
+export type AppsAttributes = NimiqAppDocument['data'] & {
+  color: `#${string}`
+}
 
 const appColor: Record<Exclude<NimiqAppDocumentData['type'], null>, string> = {
   'E-commerce': colors.blue,
@@ -15,14 +29,33 @@ const appColor: Record<Exclude<NimiqAppDocumentData['type'], null>, string> = {
   'Promotion': '#795548', // brown
 }
 
-export type SocialMediaName = Exclude<SocialMediaAttributes['platform'], null>
+export type SocialMediaName = Exclude<
+  SocialMediaAttributes['platform'],
+  null
+>
 
-export const SocialMedia: Record<SocialMediaName, SocialMediaName> = { x: 'x', telegram: 'telegram', discord: 'discord', facebook: 'facebook', github: 'github', instagram: 'instagram', nimiqForum: 'nimiqForum', reddit: 'reddit', youtube: 'youtube' }
+export const SocialMedia: Record<SocialMediaName, SocialMediaName> = {
+  x: 'x',
+  telegram: 'telegram',
+  discord: 'discord',
+  facebook: 'facebook',
+  github: 'github',
+  instagram: 'instagram',
+  nimiqForum: 'nimiqForum',
+  reddit: 'reddit',
+  youtube: 'youtube',
+}
 
 // @unocss-include
-const socialMediaConfigs: Record<SocialMediaName, Pick<SocialMediaAttributes, 'color' | 'icon'>> = {
+const socialMediaConfigs: Record<
+  SocialMediaName,
+  Pick<SocialMediaAttributes, 'color' | 'icon'>
+> = {
   x: { color: '#000', icon: 'i-nimiq:logos-twitter-mono' },
-  telegram: { color: '#0088cc', icon: 'i-nimiq:logos-telegram-mono scale-97' },
+  telegram: {
+    color: '#0088cc',
+    icon: 'i-nimiq:logos-telegram-mono scale-97',
+  },
   reddit: { color: '#ff4500', icon: 'i-nimiq:logos-reddit-mono scale-105' },
   github: { color: '#333333', icon: 'i-nimiq:logos-github-mono' },
   youtube: { color: '#ff0000', icon: 'i-nimiq:logos-youtube-mono scale-85' },
@@ -35,112 +68,173 @@ const socialMediaConfigs: Record<SocialMediaName, Pick<SocialMediaAttributes, 'c
 export const useGlobalContent = defineStore('global-content', () => {
   const { client } = usePrismic()
 
-  const { data: navigationPage } = useAsyncData('navigation', () => client.getSingle('navigation'))
-  const navigation = computed(() => navigationPage.value?.data)
-
-  const navigationBlocks = computed(() => navigation.value
-    ? [
-        {
-          areaName: 'project',
-          label: navigation.value.projectGroupName,
-          links: [...navigation.value.projectLinks, ...navigation.value.projectAdditionalFooterLinks],
-        },
-        {
-          areaName: 'tech',
-          label: navigation.value.techGroupName,
-          links: [...navigation.value.techLinks, ...navigation.value.techAdditionalFooterLinks],
-        },
-        {
-          areaName: 'apps',
-          label: navigation.value.appsGroupName,
-          links: [...navigation.value.appsLinks, ...navigation.value.appsAdditionalFooterLinks],
-        },
-        {
-          areaName: 'get-started',
-          label: navigation.value.getStartedGroupName,
-          links: [...navigation.value.getStartedLinks, ...navigation.value.getStartedAdditionalFooterLinks],
-        },
-        {
-          areaName: 'community',
-          label: navigation.value.communityGroupName,
-          links: [...navigation.value.communityLinks, ...navigation.value.communityAdditionalFooterLinks],
-        },
-        {
-          areaName: 'and-more',
-          label: navigation.value.andMoreGroupName,
-          links: navigation.value.andMoreLinks,
-        },
-      ]
-    : [])
+  // Load navigation data with useQuery
+  const { data: navigation } = useQuery({
+    key: () => ['navigation'],
+    query: async () => {
+      const res = await client.getSingle('navigation')
+      return res.data
+    },
+  })
 
   const currentDate = new Date()
-  const showHotCta = computed(() => navigation.value?.hottext
-    && (navigation.value?.hotsince ? new Date(navigation.value?.hotsince) <= currentDate : true)
-    && (navigation.value?.hotuntil ? new Date(navigation.value?.hotuntil) >= currentDate : true))
+  const showHotCta = computed(() =>
+    navigation.value?.hottext
+    && (navigation.value?.hotsince
+      ? new Date(navigation.value.hotsince) <= currentDate
+      : true)
+    && (navigation.value?.hotuntil
+      ? new Date(navigation.value.hotuntil) >= currentDate
+      : true),
+  )
   const hotCtaLink = computed(() => {
     if (!showHotCta.value)
       return
-    if (navigation.value?.hothref?.link_type !== 'Document' && navigation.value?.hothref?.link_type !== 'Web')
+    if (
+      navigation.value?.hothref?.link_type !== 'Document'
+      && navigation.value?.hothref?.link_type !== 'Web'
+    ) {
       return
+    }
     if ((navigation.value?.hothref as { type: string }).type === 'blog_page')
       return `/blog/${(navigation.value?.hothref as { uid: string }).uid}`
     return (navigation.value.hothref as { url: string }).url
   })
 
-  const { data: socialMediaPrismic } = useAsyncData('socialMedia', () => client.getByType('socialMedia'), { transform: data => data.results })
-
+  // Load social media data with useQuery
+  const socialMediaQuery = useQuery({
+    key: () => ['socialMedia'],
+    query: async () => {
+      const res = await client.getByType('socialMedia')
+      return res.results
+    },
+  })
   const socialMedias = computed(() => {
-    if (!socialMediaPrismic.value)
+    if (!socialMediaQuery.state.value.data)
       return {} as Record<SocialMediaName, SocialMediaAttributes>
-
     return Object.fromEntries(
       Object.entries(socialMediaConfigs).map(([socialMedia, config]) => {
-        const prismicData = socialMediaPrismic.value!.find(item => item.data.platform === socialMedia)
+        const prismicData = socialMediaQuery.state.value.data!.find(
+          (item: any) => item.data.platform === socialMedia,
+        )
         if (!prismicData) {
           throw new Error(`Platform data not found for ${socialMedia}`)
         }
-        return [socialMedia, { ...prismicData.data, id: prismicData.id, ...config }]
+        return [
+          socialMedia,
+          { ...prismicData.data, id: prismicData.id, ...config },
+        ]
       }),
     ) as Record<SocialMediaName, SocialMediaAttributes>
   })
 
   function getSocialMediaById(id: string) {
-    return Object.values(socialMedias.value).find(socialMedia => socialMedia.id === id)
+    return Object.values(socialMedias.value).find(
+      socialMedia => socialMedia.id === id,
+    )
   }
 
-  const { data: nimiqAppsPrismic } = useAsyncData('apps', () => client.getByType('nimiq_app'), { transform: data => data.results })
+  // Load apps data with useQuery
+  const { data: apps } = useQuery({
+    key: () => ['apps'],
+    query: async () => {
+      const res = await client.getByType('nimiq_app')
+      return res.results
+    },
+  })
   const nimiqApps = computed(() => {
-    if (!nimiqAppsPrismic.value)
+    if (!apps.value)
       return {} as Record<string, AppsAttributes>
-
     return Object.fromEntries(
-      nimiqAppsPrismic.value.map((app) => {
+      apps.value.map((app) => {
         const data = app.data
-        return [data.name, { ...data, color: data.type ? appColor[data.type] : '#000' }]
+        return [
+          data.name,
+          { ...data, color: data.type ? appColor[app.data.type] : '#000' },
+        ]
       }),
     ) as Record<string, AppsAttributes>
   })
 
   function getRandomApps(n: number) {
+    // Using Nuxt's useState is fine here.
     const randomApps = useState('random_apps', () => {
-      const apps = Object.values(nimiqApps.value || []).filter(({ logo }) => hasImage(logo))
-      return apps
-        .sort(() => Math.random() - 0.5)
-        .slice(0, Math.min(n, apps.length))
+      const apps = Object.values(nimiqApps.value || []).filter(({ logo }) =>
+        hasImage(logo),
+      )
+      return apps.sort(() => Math.random() - 0.5).slice(0, Math.min(n, apps.length))
     })
     return randomApps
   }
 
   const { supabase } = useRuntimeConfig().public
+  const getSupabaseEndpoint = (fn: string) =>
+    `${supabase.url}/rest/v1/rpc/${fn}?apikey=${supabase.key}`
 
-  const getSupabaseEndpoint = (fn: string) => `${supabase.url}/rest/v1/rpc/${fn}?apikey=${supabase.key}`
-  const { data: cryptoMapStats, error: errorGettingStats } = useAsyncData('get_stats', () => $fetch<{ locations: number }>(getSupabaseEndpoint('get_stats')))
-  const cryptoMapLocationsCount = computed(() => errorGettingStats.value ? 29_379 : cryptoMapStats.value?.locations)
-  const { data: cryptoMapContinentsStats } = useAsyncData('get_stats_for_all_continents', () => $fetch<{ locations: number }>(getSupabaseEndpoint('get_stats_for_all_continents')))
+  // Load cryptoMap stats with useQuery
+  const { data: cryptoMapStats } = useQuery({
+    key: () => ['get_stats'],
+    query: () => $fetch<{ locations: number }>(getSupabaseEndpoint('get_stats')),
+  })
+  const cryptoMapLocationsCount = computed(() => cryptoMapStats.value?.locations || 30335)
+  const { data: cryptoMapContinentsStats } = useQuery({
+    key: () => ['get_stats_for_all_continents'],
+    query: () => $fetch<{ locations: number }>(getSupabaseEndpoint('get_stats_for_all_continents')),
+  })
 
   return {
     navigation,
-    navigationBlocks,
+    navigationBlocks: computed(() =>
+      navigation.value
+        ? [
+            {
+              areaName: 'project',
+              label: navigation.value.projectGroupName,
+              links: [
+                ...navigation.value.projectLinks,
+                ...navigation.value.projectAdditionalFooterLinks,
+              ],
+            },
+            {
+              areaName: 'tech',
+              label: navigation.value.techGroupName,
+              links: [
+                ...navigation.value.techLinks,
+                ...navigation.value.techAdditionalFooterLinks,
+              ],
+            },
+            {
+              areaName: 'apps',
+              label: navigation.value.appsGroupName,
+              links: [
+                ...navigation.value.appsLinks,
+                ...navigation.value.appsAdditionalFooterLinks,
+              ],
+            },
+            {
+              areaName: 'get-started',
+              label: navigation.value.getStartedGroupName,
+              links: [
+                ...navigation.value.getStartedLinks,
+                ...navigation.value.getStartedAdditionalFooterLinks,
+              ],
+            },
+            {
+              areaName: 'community',
+              label: navigation.value.communityGroupName,
+              links: [
+                ...navigation.value.communityLinks,
+                ...navigation.value.communityAdditionalFooterLinks,
+              ],
+            },
+            {
+              areaName: 'and-more',
+              label: navigation.value.andMoreGroupName,
+              links: navigation.value.andMoreLinks,
+            },
+          ]
+        : [],
+    ),
     copyrightNotice: `© Nimiq Foundation 2017-${new Date().getFullYear()}`,
     hotCtaLink,
     socialMedias,
