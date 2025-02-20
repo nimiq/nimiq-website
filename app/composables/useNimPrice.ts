8925c9f10762f0392df1b63b4db070f8b374a27a
onmax <maximogarciamtnez@gmail.com>
2025-02-20 12:27:26 +0100

    chore: refactor Nim price history price

--- app/composables/useNimPrice.ts
+++ app/composables/useNimPrice.ts
@@ -1,27 +1,19 @@
 import { CryptoCurrency, FiatCurrency, getExchangeRates, getHistoricExchangeRates } from '@nimiq/utils'
 
 export function useNimPrice() {
-  const { data: price } = useAsyncData('price_now', async () => {
+  const { data: price } = useAsyncData<number>('price_now', async () => {
     const { nim } = await getExchangeRates([CryptoCurrency.NIM], [FiatCurrency.USD])
-    return nim.usd
+    return nim.usd! || 0
   })
 
-  const { data: price1DayAgo } = useAsyncData('price_1d', async () => {
+  const { data: price1DayAgo } = useAsyncData<number>('price_1d', async () => {
     const oneDayAgo = Date.now() - 24 * 60 * 60 * 1000
     const res = await getHistoricExchangeRates(CryptoCurrency.NIM, FiatCurrency.USD, [oneDayAgo])
     return res.get(oneDayAgo) || 0
   })
 
-  type HistoricPricesPeriod = '1y' | '2y' | '5y'
-  const selectedHistoricPricePeriod = useLocalStorage<HistoricPricesPeriod>('use_nim_price_period', '1y')
-  const historicPriceRangeOptions: HistoricPricesPeriod[] = ['1y', '2y', '5y']
-  const { data: historicPrices } = useFetch<[number, number][]>(`/api/nim-price/${selectedHistoricPricePeriod.value}`, { watch: [selectedHistoricPricePeriod] })
-
   return {
-    price,
+    price: price as Ref<number>,
     price1DayAgo,
-    selectedHistoricPricePeriod,
-    historicPriceRangeOptions,
-    historicPrices,
   }
 }
