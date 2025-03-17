aacc3c7c6495169970a6bd82c03a75f69f4610ce
onmax <maximogarciamtnez@gmail.com>
2025-03-17 18:01:29 +0100

    feat: enhance app card components with improved props and filtering functionality

--- app/components/Card/App.vue
+++ app/components/Card/App.vue
@@ -1,42 +1,27 @@
 <script setup lang="ts">
-import type { Content } from '@prismicio/client'
-
-const { type } = defineProps<NimiqApp>()
-
-// @unocss-include
-
-const appColor: { [key in Content.NimiqAppDocument['data']['type']]: string } = {
-  'Insights': 'bg-green',
-  'E-commerce': 'bg-blue',
-  'Games': 'bg-purple',
-  'Faucet': 'bg-pink',
-  'Promotion': 'bg-brown',
-  'Miner': 'bg-darkblue',
-  'Wallets': 'bg-orange',
-  'Infrastructure': 'bg-red',
-  'Bots': 'bg-gold',
-}
-
-const bgColor = computed(() => appColor[type])
+defineProps<{ color: string, link: string, logo: string, type: string, name: string, description: string, developer?: string }>()
 </script>
 
 <template>
-  <NuxtLink :to="link" target="_blank" external h-full gap-24 rounded-6 p-6 nq-hoverable w="[min(calc(100vw-var(--px,32px)*2),350px)]">
-    <div :class="bgColor" mb-0 h-240 rounded-4 stack>
-      <NuxtImg :src="logo!" h-auto max-h-full max-w-full rounded-4 object-cover op-60 />
+  <NuxtLink :to="link" target="_blank" w="[min(calc(100vw-var(--px,32px)*2),350px)]" :style="`--c: ${color}`" external group h-full gap-24 rounded-6 p-6 nq-hoverable>
+    <!-- TODO Fix stack -->
+    <div bg="$c" mb-0 h-240 rounded-4 stack>
+      <NuxtImg :src="logo" h-auto max-h-full max-w-full rounded-4 object-cover op-60 />
       <p text="12 white/70" relative right-12 top-12 self-start justify-self-end nq-label>
         {{ type }}
       </p>
     </div>
-    <div nq-prose-compact flex-1 p-26 pt-0 flex="~ col gap-12">
+    <div flex-1 p-26 pt-0 flex="~ col gap-12">
       <h3>{{ name }}</h3>
-      <p nq-prose-compact f-pt-2xs f-pt-sm>
+      <p text="neutral-800 group-hover:neutral-900" nq-prose-compact mt-0 transition-colors f-pt-2xs>
         {{ description }}
       </p>
 
-      <p v-if="developer" mt-auto text-14 nq-label>
-        By <span text-blue>{{ developer }}</span>
-      </p>
+      <div class="nq-hoverable-cta" mt-auto w-max>
+        <p v-if="developer" text-14 nq-label>
+          By <span text-blue>{{ developer }}</span>
+        </p>
+      </div>
     </div>
   </NuxtLink>
 </template>
--- app/components/Card/Highlighted.vue
+++ app/components/Card/Highlighted.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-defineProps<NimiqApp>()
+const { link } = defineProps<{ name: string, description: string, screenshot?: string, link: string }>()
+
+// If url is same as current page, resolve it to avoid page reload
+const url = new URL(link)
+const resolvedUrl = url.host === useRequestURL().host ? url.pathname : link
 </script>
 
 <template>
-  <div flex="~ wrap items-center gap-8" h-full rounded-6 bg-neutral-0 shadow f-pt-xs>
-    <div w-320 rounded-4 stack>
-      <NuxtImg v-if="screenshot" :src="screenshot!" h-auto max-h-full max-w-full rounded-4 object-cover />
-    </div>
-    <div flex="~ col gap-12" nq-prose-compact flex-1 p-24>
+  <div flex="~ wrap items-center gap-8" h-full rounded-6 bg-neutral-0 shadow f-p-xs>
+    <NuxtImg v-if="screenshot" :src="screenshot!" h-auto max-h-full max-w-full w-320 rounded-4 rounded-8 object-cover />
+    <div flex="~ col gap-12" flex-1 p-24>
       <h3>{{ name }}</h3>
-      <p nq-prose-compact f-pt-2xs>
+      <p class="nq-raw" mt-0 text-neutral-800 f-pt-2xs>
         {{ description }}
       </p>
-      <NuxtLink :href="link" nq-arrow nq-pill-blue f-mt-xl>
+      <NuxtLink :to="resolvedUrl" mx-0 nq-arrow nq-pill-lg nq-pill-blue f-mt-md>
         Learn more
       </NuxtLink>
     </div>
