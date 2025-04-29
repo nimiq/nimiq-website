4979daa38af89aa3f43d089dbd5b011c69ba780a
onmax <maximogarciamtnez@gmail.com>
2025-04-29 08:08:56 +0200

    feat: replace useExchanges with API endpoint for fetching exchange data

--- app/slices/ExchangesGrid/index.vue
+++ app/slices/ExchangesGrid/index.vue
@@ -3,15 +3,15 @@ import type { Content } from '@prismicio/client'
 
 defineProps(getSliceComponentProps<Content.ExchangesGridSlice>())
 
-const { data: exchanges } = await useExchanges()
+const { data: exchanges } = await useFetch('/api/exchanges')
 </script>
 
 <template>
   <section bg-neutral-0>
     <ul grid="~ cols-[repeat(auto-fit,minmax(200px,368px))] gap-16 justify-center">
-      <li v-for="({ url, logo, name }, i) in exchanges" :key="i">
-        <NuxtLink external :to="url" flex="~ row items-center gap-x-16" nq-hoverable target="_blank">
-          <NuxtImg :src="logo!" h-full w-40 object-contain flex="~ items-center" />
+      <li v-for="({ link, logo, name }, i) in exchanges" :key="i">
+        <NuxtLink external :to="link" flex="~ row items-center gap-x-16" nq-hoverable target="_blank">
+          <PrismicImage :field="logo!" h-full w-40 object-contain flex="~ items-center" />
           <h3 font-semibold f-text-xl>
             {{ name }}
           </h3>
---
+++ server/api/exchanges.get.ts
@@ -0,0 +1,26 @@
+import type { ImageField } from '@prismicio/client'
+import type { AllDocumentTypes } from '~~/prismicio-types'
+import { createClient } from '@prismicio/client'
+import { getLink } from '~/composables/usePrismicHelper'
+
+export default cachedEventHandler(async () => {
+  const clientConfig = useRuntimeConfig().public.prismic?.clientConfig
+  if (!clientConfig)
+    throw createError({ statusCode: 500, statusMessage: 'Prismic options not found' })
+  const client = createClient<AllDocumentTypes>('nimiq', clientConfig)
+  const data = await client.getAllByType('exchange', {
+    orderings: [{
+      field: 'last_publication_date',
+      direction: 'desc',
+    }],
+    pageSize: 100,
+  }).then(res => res.map(exchange => ({
+    link: getLink(exchange.data.link),
+    name: exchange.data.name,
+    logo: exchange.data.logo satisfies ImageField,
+  })))
+  return data
+}, {
+  getKey: () => 'exchanges',
+  maxAge: 60 * 60 * 6, // 24 hours
+})
