1a35f314a7cda7f34af5bba66f4dd0c440cc3102
onmax <maximogarciamtnez@gmail.com>
2025-05-06 16:05:46 +0200

    feat: enhance LockBadge component to display environment name and improve draft visibility

--- app/components/LockBadge.vue
+++ app/components/LockBadge.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+const { withEnv = false } = defineProps<{ withEnv?: boolean }>()
+
+const { isProduction, name } = useRuntimeConfig().public.environment
+</script>
+
 <template>
-  <div title="This item is a draft in the CMS. This will be hidden in production" ring="1.5 white" nq-pill-lg nq-pill-orange>
-    <div i-nimiq:locked-lock />
-    Draft
+  <div v-if="!isProduction" flex="~ items-center">
+    <Tooltip v-if="withEnv">
+      <template #trigger>
+        <div ring="1.5 white" nq-pill-lg nq-pill-orange f-text-2xs>
+          Deployed {{ name }}
+        </div>
+      </template>
+    </Tooltip>
+    <div title="This item is a draft in the CMS. This will be hidden in production" ring="1.5 white" nq-pill-lg nq-pill-orange>
+      <div i-nimiq:locked-lock />
+      Draft
+    </div>
   </div>
 </template>
