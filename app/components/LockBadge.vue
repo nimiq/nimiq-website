6836d7e0acf52365692f9860378f8a6cde1ccd3a
onmax <maximogarciamtnez@gmail.com>
2025-05-06 16:52:00 +0200

    feat: add draft environment visibility message to LockBadge component

--- app/components/LockBadge.vue
+++ app/components/LockBadge.vue
@@ -12,6 +12,14 @@ const { isProduction, name } = useRuntimeConfig().public.environment
           Deployed {{ name }}
         </div>
       </template>
+      <template #content>
+        <p>
+          You are previewing the <strong>{{ name }}</strong> environment. This is a development-only message.
+        </p>
+        <p>
+          This environment is not intended for production use and may contain incomplete or unstable features. <a href="https://github.com/nimiq/nimiq-website?tab=readme-ov-file#runtime-environments" target="_blank" nq-arrow>Learn more</a>
+        </p>
+      </template>
     </Tooltip>
     <div v-if="draft" title="This item is a draft in the CMS. This will be hidden in production" ring="1.5 white" nq-pill-lg nq-pill-orange>
       <div i-nimiq:locked-lock />
