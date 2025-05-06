71a9bb9467ef6b351e439797081752e46930cbd3
onmax <maximogarciamtnez@gmail.com>
2025-05-06 07:49:16 +0200

    feat: refactor PillSelector component for improved performance and style transitions

--- app/components/PillSelector.vue
+++ app/components/PillSelector.vue
@@ -1,49 +1,41 @@
 <script setup lang="ts" generic="T">
-import { onMounted, ref, watch } from 'vue'
+import type { CSSProperties } from 'vue'
+import { nextTick, ref, watch } from 'vue'
 
-defineProps<{
-  options: T[]
-}>()
+defineProps<{ options: T[] }>()
 
 const model = defineModel<T>()
+const isMounted = useMounted()
 
-const loaded = ref(false)
+const labels = useTemplateRef('labels')
+const pillStyles = ref<CSSProperties>()
 
-const id = useId()
-
-onMounted(() => {
-  setPill()
-  setTimeout(() => loaded.value = true, 300)
-})
-watch(model, setPill, { immediate: true })
-onMounted(setPill)
-
-const pillStyles = ref({ left: '0px', width: '0px' })
-function setPill() {
-  if (import.meta.server)
+watch(() => model.value, async () => {
+  await nextTick()
+  const activeLabel = labels.value?.find(label => label.id === String(model.value))
+  labels.value?.forEach(label => label.dataset.state = activeLabel && label.isEqualNode(activeLabel) ? 'active' : undefined)
+  if (!activeLabel)
     return
-  // Use String() to ensure proper comparison for all value types
-  const selectedLabel = globalThis.document?.querySelector(`#${id}-${String(model.value)}`)?.parentElement as HTMLLabelElement
-  if (!selectedLabel)
-    return
-  globalThis.window?.requestAnimationFrame(() => {
-    pillStyles.value = { left: `${selectedLabel.offsetLeft}px`, width: `${selectedLabel.offsetWidth}px` }
-  })
-}
+  pillStyles.value = {
+    left: `${activeLabel.offsetLeft}px`,
+    width: `${activeLabel.offsetWidth}px`,
+    transition: isMounted ? 'left 300ms, width 200ms' : 'none',
+  }
+}, { immediate: true })
 </script>
 
 <template>
   <div flex="~ align-center" relative w-max rounded-full bg-neutral-200 p-2>
+    <!-- {{ pillStyles }} -->
     <label
       v-for="(option, i) of options"
-      :key="i" :data-state="String(model) === String(option) ? 'active' : undefined"
+      :id="String(option)" ref="labels" :key="i"
       text="12 neutral-800 data-active:neutral hocus:neutral-900" flex="~ items-center" relative z-2 h-max cursor-pointer select-none rounded-full px-8 py-4 lh-none transition-colors nq-label
     >
-      {{ { model, option } }}
-      <input :id="String(option)" v-model="model" type="radio" :value="option" sr-only @mousedown.prevent>
+      <input v-model="model" type="radio" :value="option" sr-only @mousedown.prevent>
       {{ option }}
     </label>
-    <div absolute top-2 z-1 h-20 rounded-full bg-neutral-0 :style="{ ...pillStyles, transition: loaded ? 'left 300ms, width 200ms' : '' }" />
+    <div absolute top-2 z-1 h-20 rounded-full bg-neutral-0 :style="pillStyles" />
   </div>
 </template>
 
