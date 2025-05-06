0afbc6cebeae06178c01a1639a61e9848f33392b
onmax <maximogarciamtnez@gmail.com>
2025-05-06 16:44:55 +0200

    feat: enhance LockBadge component to support draft visibility

--- app/components/LockBadge.vue
+++ app/components/LockBadge.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-const { withEnv = false } = defineProps<{ withEnv?: boolean }>()
+const { withEnv = false, draft = false } = defineProps<{ withEnv?: boolean, draft?: boolean }>()
 
 const { isProduction, name } = useRuntimeConfig().public.environment
 </script>
 
 <template>
-  <div v-if="!isProduction" flex="~ items-center">
+  <div v-if="!isProduction && (draft || withEnv)" flex="~ items-center">
     <Tooltip v-if="withEnv">
       <template #trigger>
         <div ring="1.5 white" nq-pill-lg nq-pill-orange f-text-2xs>
@@ -13,7 +13,7 @@ const { isProduction, name } = useRuntimeConfig().public.environment
         </div>
       </template>
     </Tooltip>
-    <div title="This item is a draft in the CMS. This will be hidden in production" ring="1.5 white" nq-pill-lg nq-pill-orange>
+    <div v-if="draft" title="This item is a draft in the CMS. This will be hidden in production" ring="1.5 white" nq-pill-lg nq-pill-orange>
       <div i-nimiq:locked-lock />
       Draft
     </div>
