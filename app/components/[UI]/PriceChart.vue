3ec7202ca341ddf86cfbb30571ea05b0af3543c6
onmax <maximogarciamtnez@gmail.com>
2025-07-08 11:01:24 +0200

    chore: small ui tweaks
    closes #57

--- app/components/[UI]/PriceChart.vue
+++ app/components/[UI]/PriceChart.vue
@@ -94,7 +94,7 @@ const error = computed(() => {
       <div grid="~ cols-1 md:cols-[max-content_1fr]" size-full relative of-hidden>
         <aside md:border="r-1 solid neutral-400" grid="~ cols-[repeat(4,1fr)] md:cols-1 gap-col-20 gap-row-24" w-full relative f-p-md max-md:row-start-2 max-md:of-x-auto>
           <transition enter-active-class="transition duration-200 ease-out" enter-from-class="op-0" enter-to-class="op-100" leave-active-class="transition duration-200 ease-out" leave-from-class="op-100" leave-to-class="op-0">
-            <div v-if="isLoading || error" flex="~ items-center gap-8" text=" gold f-sm" translate-x="100%" py-4 rounded-br-6 bg-white right--1 top--1 absolute z-30 f-px-xs border="b r neutral-400">
+            <div v-if="isLoading || error" flex="~ items-center gap-8" text=" orange f-sm" translate-x="100%" py-4 rounded-br-6 bg-white right--1 top--1 absolute z-30 f-px-xs border="b r neutral-400" lg:w-max>
               <div scale-90 :class="isLoading ? 'i-nimiq:spinner' : 'i-nimiq:alert'" />
               <p>
                 {{ isLoading ? 'Loading...' : error }}
