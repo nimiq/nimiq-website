06936408ea6082159a725c92290adf896d929982
onmax <maximogarciamtnez@gmail.com>
2025-08-03 10:18:07 +0700

    chore: force light mode generally

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,11 @@
-<script>
-if (import.meta.dev)
-  useDark() // Take care of you eyes ;)
-</script>
-
 <template>
   <div>
     <NuxtPage />
   </div>
 </template>
+
+<style>
+body {
+  color-scheme: light;
+}
+</style>
