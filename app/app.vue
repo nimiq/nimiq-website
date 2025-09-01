46e799a064666c15d29ffa2be916a301538024de
onmax <maximogarciamtnez@gmail.com>
2025-09-01 08:37:17 +0200

    chore: disable nuxthub preview

--- app/app.vue
+++ app/app.vue
@@ -4,9 +4,16 @@ const showConsentBanner = environment.isProduction || enableDevAnalytics
 </script>
 
 <template>
-  <div>
+  <div v-if="!environment.isNuxthubPreview && !environment.isNuxthubPreview">
+    ">
     <NuxtPage />
     <ConsentBanner v-if="true || showConsentBanner" />
+  </div>
+  <div v-else>
+    This is a limited environment for previewing Nuxthub. Please visit
+    <NuxtLink href="https://nimiq.com" target="_blank" class="underline">
+      nimiq.com
+    </NuxtLink> for the full experience.
   </div>
 </template>
 
