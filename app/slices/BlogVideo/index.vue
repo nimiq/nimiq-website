364386fcd9b89ab1080a1a8092b2a67fe0a8f7ec
onmax <maximogarciamtnez@gmail.com>
2026-02-02 12:17:54 +0000

    feat: add BlogVideo slice for video support in blog posts
    - Create BlogVideo slice with video URL, poster image, caption, autoplay and loop options
    - Add ContentBlogVideo component with play button overlay for poster images
    - Register slice in blog_page custom type
    - Add slice to prismic-sync script for content migration
    
    Allows embedding videos between rich text sections in blog posts.
    Videos should be uploaded to Prismic media library (not committed to git).

---
+++ app/slices/BlogVideo/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import type { Content } from '@prismicio/client'
+
+const { slice } = defineProps(getSliceComponentProps<Content.BlogVideoSlice>())
+</script>
+
+<template>
+  <ContentBlogVideo :slice="slice as any" />
+</template>
