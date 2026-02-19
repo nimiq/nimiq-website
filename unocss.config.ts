737ab799fa06ef3b4191bb87e3fed1d744c232db
onmax <maximogarciamtnez@gmail.com>
2026-02-19 09:24:05 +0100

    fix(unocss): order static-content layer

--- unocss.config.ts
+++ unocss.config.ts
@@ -7,7 +7,7 @@ import { presetOnmax } from 'unocss-preset-onmax'
 
 export default defineConfig({
   preflights: [
-    { getCSS: () => '@layer nq-preflight, nq-typography, nq-utilities, nq-variants;' },
+    { getCSS: () => '@layer nq-preflight, nq-static-content, nq-typography, nq-utilities, nq-variants;' },
   ],
   rules: [
     [/^area-(.*)$/, ([, t]) => ({ 'grid-area': t })],
