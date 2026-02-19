d9de0466c830912224fc71bbe8ee5b1f75ac1c98
onmax <maximogarciamtnez@gmail.com>
2026-02-19 16:27:59 +0100

    fix(css): restore pre-today blog styles

--- app/pages/blog/[post].vue
+++ app/pages/blog/[post].vue
@@ -30,7 +30,7 @@ if (post.value?.data.body.at(0)?.primary) {
 
 <template>
   <NuxtLayout v-if="post">
-    <div ref="articleRef" data-blog-post>
+    <div ref="articleRef">
       <PageInfo :draft bottom-32 right-32 fixed z-102 />
 
       <header data-section max-w="$nq-prose-max-width" f-pt="96/136" px="32 lg:64" pb-0>
@@ -48,9 +48,7 @@ if (post.value?.data.body.at(0)?.primary) {
       </header>
     </div>
     <SliceZone v-if="post.data.body.length > 0" :slices="post.data.body" :components />
-    <div v-else data-blog-post-prose>
-      <RichText nq-prose wrapper="article" :field="post.data.text" />
-    </div>
+    <RichText v-else nq-prose wrapper="article" :field="post.data.text" />
     <Disclaimer />
   </NuxtLayout>
 </template>
@@ -66,68 +64,19 @@ if (post.value?.data.body.at(0)?.primary) {
   --uno: 'z-101 rounded-8 mt-0 bg-white';
 }
 
-:global([data-blog-post] [nq-prose] > *:not(img, video, iframe, figure, blockquote, pre, code)) {
+:global([nq-prose] > *:not(img, video, iframe, figure, blockquote, pre, code)) {
   --uno: 'px-0';
 }
 
-:global([data-blog-post] [nq-prose] > :where(h2, h3, h4, h5, h6) > *) {
+:global([nq-prose] > :where(h2, h3, h4, h5, h6) > *) {
   --uno: 'text-1em';
 }
 
-:global([data-blog-post] [nq-prose] :where(h2, h3, h4, h5, h6) > :where(strong, b)),
-:global([data-blog-post] [nq-prose] :where(h2, h3, h4, h5, h6) :where(strong, b) > *) {
-  font-size: 1em;
-  line-height: inherit;
+:global([nq-prose] > :where(h2, h3, h4, h5, h6):not(:first-child)) {
+  --uno: 'mt-1.5lh';
 }
 
-:global([data-blog-post] [nq-prose] > :where(h2, h3, h4, h5, h6):not(:first-child)) {
-  margin-top: 1lh;
-}
-
-:global([data-blog-post] [nq-prose] :where(h2)) {
-  --font-size-min-em: 1.5;
-  --font-size-max-em: 1.7;
-  line-height: 1.25;
-  margin-bottom: 0.75lh;
-}
-
-:global([data-blog-post] [nq-prose] :where(h3)) {
-  --font-size-min-em: 1.25;
-  --font-size-max-em: 1.4;
-  line-height: 1.25;
-  margin-bottom: 0.6lh;
-}
-
-:global([data-blog-post] [nq-prose] :where(h4)) {
-  --font-size-min-em: 1.1;
-  --font-size-max-em: 1.25;
-  line-height: 1.25;
-  margin-bottom: 0.5lh;
-}
-
-:global([data-blog-post] [nq-prose] :where(strong, b):not(:where(h1, h2, h3, h4, h5, h6) *)) {
-  font-weight: 700;
-  color: color-mix(in srgb, rgb(var(--nq-neutral-800)) 50%, rgb(var(--nq-neutral-900)) 50%);
-}
-
-:global([data-blog-post] [nq-prose] > :where(ul, ol) > li) {
-  margin-left: 20px;
-}
-
-:global([data-blog-post] [nq-prose] > ul > li::before) {
-  top: 0.6em;
-}
-
-:global([data-blog-post] [nq-prose] > ol > li::before) {
-  top: 0.15em;
-}
-
-:global([data-blog-post] [nq-prose] > :where(ul, ol)) {
-  margin-top: 16px;
-  margin-bottom: 16px;
-}
-
-:global([data-blog-post] section:has([nq-prose])) {
+:global(section:has(article[nq-prose])) {
   --uno: 'f-pt-2xl';
 }
 </style>
